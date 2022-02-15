438a499f1e7d4143c3b2153e1d2a14490ac8b963
nathan <31683427+nathonNot@users.noreply.github.com>
2022-02-15 18:14:56 +0800

    Create AutoChess.sol

--- src/contracts/AutoChess.sol
+++ src/contracts/AutoChess.sol
@@ -3,21 +3,28 @@ pragma solidity ^0.8.0;
 
 import "./MiracleCard.sol";
 import "../lib/AutoChessEntryFunc.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract GameAutoCheess {
-    MiracleCard public cardNFT;
-
+    using SafeMath for uint256;
     using AutoChessEntryFunc for AutoChessEntryFunc.EntryFunc;
+    using EnumerableSet for EnumerableSet.UintSet;
+    MiracleCard public cardNFT;
     AutoChessEntryFunc.EntryFunc private typeFunction;
+    EnumerableSet.UintSet private _holderAddress;
 
     constructor(address card) {
         cardNFT = MiracleCard(card);
         typeFunction.init();
     }
 
-    function challenge() public{
-        typeFunction.dispatch(1);
+    function Authorize() public {
+        _holderAddress.add(uint256(uint160(msg.sender)));
     }
 
-
+    function challenge() public {
+        typeFunction.dispatch(1);
+    }
 }
